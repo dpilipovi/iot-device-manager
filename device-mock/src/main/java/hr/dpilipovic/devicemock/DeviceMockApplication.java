43200752e4ebdbfe37da0604850854c9c275de54
dpilipovic <david.pilipovic@biss.hr>
2022-09-11 22:45:18 +0200

    Removed unnecessary dependencies and database connection from device-mock

--- device-mock/src/main/java/hr/dpilipovic/devicemock/DeviceMockApplication.java
+++ device-mock/src/main/java/hr/dpilipovic/devicemock/DeviceMockApplication.java
@@ -1,11 +1,8 @@
 package hr.dpilipovic.devicemock;
 
-import hr.dpilipovic.common.configuration.CommonDataConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@Import(CommonDataConfiguration.class)
 @SpringBootApplication
 public class DeviceMockApplication {
 
